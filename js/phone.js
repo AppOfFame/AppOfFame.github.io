92652799e10c519c9363e8a916aca5d370d0127a
mael25 <mael18@windowslive.com>
2020-05-11 18:48:57 +0200

    better display v.2

--- js/phone.js
+++ js/phone.js
@@ -10,12 +10,13 @@ function whenDocumentLoaded(action) {
 let iphone;
 whenDocumentLoaded(() => {
 	//Each element will correspond to an app cell in the phone
-	data = [ {name:'Rankings', icon:'facebook.png', file:'app_fb'},
+	data = [ {} ,{},{},{},{},{},{},{},{},
+	{name:'Rankings', icon:'facebook.png', file:'app'},
 	{name:'Distributions', icon:'whatsapp.png', file:'app_whats'},
 	{name:'Developers', icon:'EPFL.png', file:'app_epfl'},
 	{name:'Downloads', icon:'youtube.png', file:'app_ytb'},
 	{name:'Comments', icon:'twitter.png', file:'app_twitter'},
-	{} ];
+	{name:'Conclusion', icon:'wikipedia.png', file:''}];
 
 	iphone = new Phone('monTel', data, [14,9]);
 	window.addEventListener("resize", windowUpdate);
@@ -45,7 +46,7 @@ class Phone {
 		this.contHeight = document.getElementById(this.container).clientHeight;
 		this.nbCol=Math.round(this.ratio[1]*Math.sqrt(data.length/(this.ratio[0]*this.ratio[1])));
 		let sizeFromWidth = this.contWidth/((3*this.nbCol+1)/2);
-		let sizeFromHeight = this.contHeight/((3*Math.ceil(data.length/this.nbCol)+1)/2);
+		let sizeFromHeight = this.contHeight/((3*Math.ceil(data.length/this.nbCol)+3)/2);
 		if(sizeFromWidth<sizeFromHeight)
 		{
 			this.size = sizeFromWidth;
@@ -60,10 +61,19 @@ class Phone {
 			this.height = this.contHeight;
 			this.width = this.margin + this.nbCol*(this.size+this.margin);
 		}
-		this.offsetX = (this.contWidth-this.width)/2;
+		this.topBarHeight=30;
+		this.offsetX = (this.contWidth-this.width)/2 ;
 		this.offsetY = (this.contHeight-this.height)/2;
 		this.webpage='';
+		this.bSize = 0.04*this.height;
+		this.bPadding = 0.01*this.height;
+		this.padding = 3;
 
+
+		let topBar = document.getElementById("topBar");
+		topBar.style.width = this.width-29+"px";
+		topBar.style.marginLeft = this.offsetX+1.5*this.padding+"px";
+		topBar.style.marginTop = this.offsetY+1.5*this.padding+"px";
 		this.rescaleContent();
 
 		this.update(this);
@@ -72,13 +82,13 @@ class Phone {
 
 	rescaleContent()
 	{
-		let padding = 3;
 		let contentDiv = document.getElementById("content");
-		contentDiv.style.width = this.width-2*padding+"px";
-		//contentDiv.style.height = this.height-this.margin-padding+"px";
-		contentDiv.style.height = this.height-2*padding+"px";
-		contentDiv.style.marginLeft = this.offsetX+padding+"px";
-		contentDiv.style.marginTop = this.offsetY+padding+"px";
+		contentDiv.style.width = this.width-2*this.padding+"px";
+		//contentDiv.style.height = this.height-this.margin-this.padding+"px";
+		contentDiv.style.height = this.height-2*this.padding+"px";
+		contentDiv.style.marginLeft = this.offsetX+this.padding+"px";
+		contentDiv.style.marginTop = this.offsetY+this.padding+"px";
+		let frame = d3.selectAll(".frame").attr("height", this.height-2*this.bSize-2*this.bPadding);
 	}
 
 
@@ -128,7 +138,7 @@ update(phone)
 
 	let svg = d3.select('#'+this.container);
 	let borderPad=2.5;
-  const rectSize = 0.04*this.height;
+
 
 	//Appends app icon when new app enters
 	function appendIcon(phone, icon)
@@ -157,13 +167,14 @@ update(phone)
 	.enter()
 	.append('rect')
 	.attr('x', (d,i) => this.offsetX + this.margin + (i%this.nbCol)*(this.size+this.margin))
-	.attr('y', (d,i) => this.offsetY + this.margin/2 + Math.floor(i/this.nbCol)*(this.size+this.margin))
-	.attr('rx', '2em')
-	.attr('ry', '2em')
+	.attr('y', (d,i) => this.offsetY + this.margin/2 + this.topBarHeight + Math.floor(i/this.nbCol)*(this.size+this.margin))
+	.attr('rx', 0.3*this.size)
+	.attr('ry', 0.3*this.size)
 	.attr('width', this.size)
 	.attr('height', this.size)
 	.attr("fill", (d) => appendIcon(phone, d.icon))
 	.attr('class','borderHighlight temporary')
+	.style('visibility', (d) => d.icon? "visible" : "hidden")
 
 
 
@@ -202,17 +213,18 @@ this.active = false;
 		let contentDiv = document.getElementById("content");
 		contentDiv.classList.remove("offStage");
 		contentDiv.classList.add("onStage");
-		contentDiv.innerHTML='<iframe class="frame" src="html/'+phone.page+'.html"></iframe>'
-		let frame = d3.selectAll(".frame").attr("height", phone.height-2*rectSize-2*bPadding);
+		contentDiv.innerHTML='<iframe class="frame" src="html/'+phone.page+'.html"></iframe>';
+		let frame = d3.selectAll(".frame").attr("height", phone.height-2*phone.bSize-2*phone.bPadding);
+		topBar.classList.add("invisible");
 
 
 		//contentDiv.innerHTML='<object type="text/html" data="app.html"></object>'
 
 		// ANIMER ICI
 		//jQuery(function($) {
-    //  $("#content").css({"display":"inline-block"});
+		//  $("#content").css({"display":"inline-block"});
 		//	$("#content").html('<iframe class="frame" src="'+phone.page+'.html"></iframe>');
-  	//});
+		//});
 
 		phone.rescaleContent();
 		let appAndText = d3.selectAll(".temporary").attr("style", "visibility:hidden;");
@@ -226,14 +238,14 @@ svg.selectAll('text')
 .append('text')
 .attr('class', 'appText temporary')
 .attr('x', (d,i) => this.offsetX + this.margin + (i%this.nbCol)*(this.size+this.margin))
-.attr('y', (d,i) => this.offsetY + this.margin/2 + Math.floor(i/this.nbCol)*(this.size+this.margin) +
-this.size + this.margin/3)
+.attr('y', (d,i) => this.offsetY + this.margin/2 + this.topBarHeight + Math.floor(i/this.nbCol)*(this.size+this.margin) +
+this.size + this.margin/2)
 .text((d,i) => d.name)
 
 //Center the texts
 d3.selectAll(".appText").each(function(d,i) {
 	d3.select(this)
-	  .attr("x", 	+d3.select(this).attr('x') + (phone.size-this.getComputedTextLength())/2);
+	.attr("x", 	+d3.select(this).attr('x') + (phone.size-this.getComputedTextLength())/2);
 });
 
 
@@ -255,22 +267,20 @@ svg.append('rect')
 
 //Buttons
 
-const bPadding = 0.01*this.height;
 let buttons = d3.select('#buttons')
-	.attr('transform', 'translate('+this.offsetX+','+(this.offsetY+ this.height - 2*rectSize - bPadding)+')')
-	//.attr('transform', 'translate(400,100)')
-	.attr('width', this.width)
-	.attr('height', rectSize+2*bPadding);
+.attr('transform', 'translate('+this.offsetX+','+(this.offsetY+ this.height - 2*this.bSize - this.bPadding/3)+')')
+//.attr('transform', 'translate(400,100)')
+.attr('width', this.width)
+.attr('height', this.bSize+2*this.bPadding);
 
 
 //Circle button
-const spaceBtwButtons = 3/2*this.margin;
+//const spaceBtwButtons = 3/2*this.margin;
+const spaceBtwButtons = 2*this.margin;
 buttons.append('circle')
 .attr('cx',  this.width/2)
-.attr('cy', bPadding+rectSize/2)
-.attr('r', 5*rectSize/8)
-.style('stroke', 'LightGray')
-.style('stroke-width', 2)
+.attr('cy', this.bPadding+this.bSize/2)
+.attr('r', 5*this.bSize/8)
 .attr('class','highlight')
 .on('click', function(){ let contentDiv = document.getElementById("content");
 //Empty webpage div display
@@ -278,6 +288,8 @@ contentDiv.innerHTML = '';
 //Make it invisible so that the emptied div does not block click
 contentDiv.classList.remove("onStage");
 contentDiv.classList.add("offStage");
+topBar.classList.remove("invisible");
+phone.page=null;
 
 phone.flipToPhone();
 });
@@ -285,39 +297,39 @@ phone.flipToPhone();
 //Rectangle button
 
 buttons.append('rect')
-.attr('x',  this.width/2 + spaceBtwButtons - rectSize/2)
-//.attr('y', this.height - this.margin/2 - rectSize/2)
-.attr('y', bPadding)
-.attr('width', rectSize)
-.attr('height', rectSize)
-.style('stroke', 'LightGray')
-.style('stroke-width', 2)
+.attr('x',  this.width/2 + spaceBtwButtons - this.bSize/2)
+//.attr('y', this.height - this.margin/2 - this.bSize/2)
+.attr('y', this.bPadding)
+.attr('width', this.bSize)
+.attr('height', this.bSize)
 .attr('class','highlight');
 
-// Triangle button
+// §ngle button
 const lineGenerator = d3.line()
 .x(d => d.x)
 .y(d => d.y);
 
 //const triWidth =  3*this.size/16;
 //const triHeight = this.size/4;
-const triWidth =  rectSize;
-const triHeight = 4*rectSize/3;
+const triWidth =  this.bSize;
+const triHeight = 4*this.bSize/3;
 const triStart = [ this.width/2 - spaceBtwButtons + triWidth/2,
-	(rectSize-triHeight)/2+bPadding];
+	(this.bSize-triHeight)/2+this.bPadding];
 	const triData = [ {"x": triStart[0], "y": triStart[1]},
 	{"x": triStart[0], "y": triStart[1]+triHeight},
 	{"x": triStart[0]-triWidth, "y": triStart[1]+triHeight/2},
 	{"x": triStart[0], "y": triStart[1]}];
 	buttons.append("path")
 	.attr("d", lineGenerator(triData))
-	.style('stroke', 'LightGray')
-	.attr("stroke-width", 2)
 	.attr('class','highlight')
 	.on('click', function (){
-		let contentDiv = document.getElementById("content");
-		contentDiv.innerHTML='<iframe class="frame" src="html/'+phone.page+'.html"></iframe>'
-		phone.rescaleContent();
+
+		if (phone.page)
+		{
+			let contentDiv = document.getElementById("content");
+			contentDiv.innerHTML='<iframe class="frame" src="html/'+phone.page+'.html"></iframe>'
+			phone.rescaleContent();
+		}
 	});
 
 
