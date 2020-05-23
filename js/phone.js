3608923142d20c05f2ff1e033a8ee4be4ac7bb87
mael25 <mael18@windowslive.com>
2020-05-23 17:12:51 +0200

    integrated lock

--- js/phone.js
+++ js/phone.js
@@ -1,213 +1,203 @@
-function whenDocumentLoaded(action) {
-	if (document.readyState === "loading") {
-		document.addEventListener("DOMContentLoaded", action);
-	} else {
-		// `DOMContentLoaded` already fired
-		action();
-	}
-}
-
-let iphone;
-whenDocumentLoaded(() => {
-	//Each element will correspond to an app cell in the phone
-	data = [ {} ,{},{},{},{},{},{},{},{},
-	{name:'Rankings', icon:'facebook.png', file:'app_fb'},
-	{name:'Distributions', icon:'whatsapp.png', file:'app_whats'},
-	{name:'Developers', icon:'EPFL.png', file:'app_epfl'},
-	{name:'Downloads', icon:'youtube.png', file:'app_ytb'},
-	{name:'Comments', icon:'twitter.png', file:'app_twitter'},
-	{name:'Conclusion', icon:'wikipedia.png', file:'app_wiki'}];
-
-	iphone = new Phone('monTel', data, [14,9]);
-	window.addEventListener("resize", windowUpdate);
-});
-
 function windowUpdate()
 {
-	console.log("YES");
 	iphone.empty();
 	iphone.rescale();
 }
 
+function initPhone()
+{
+	data = [ {} ,{},{},{},{},{},{},{},{},
+		{name:'Rankings', icon:'facebook.png', file:'app_fb'},
+		{name:'Distributions', icon:'whatsapp.png', file:'app_whats'},
+		{name:'Developers', icon:'EPFL.png', file:'app_epfl'},
+		{name:'Downloads', icon:'youtube.png', file:'app_ytb'},
+		{name:'Comments', icon:'twitter.png', file:'app_twitter'},
+		{name:'Conclusion', icon:'wikipedia.png', file:'app_wiki'}];
 
-class Phone {
-	constructor(container, data, ratio)
-	{
-		this.container=container;
-		this.data=data;
-		this.ratio = ratio;
-		this.padding = 3;
-		this.idDiscover = 9;
-		this.topBarHeight=30;
+		iphone = new Phone('monTel', data, [14,9]);
+		window.addEventListener("resize", windowUpdate);
+		return iphone;
+	}
+
+
+	class Phone {
+		constructor(container, data, ratio)
+		{
+			this.container=container;
+			this.data=data;
+			this.ratio = ratio;
+			this.padding = 3;
+			this.idDiscover = 9;
+			this.topBarHeight=30;
+			this.rescale();
+		}
+
+		rescale()
+		{
+			console.log("RESCALED");
+			this.contWidth = document.getElementById(this.container).clientWidth;
+			this.contHeight = document.getElementById(this.container).clientHeight;
+			this.nbCol=Math.round(this.ratio[1]*Math.sqrt(data.length/(this.ratio[0]*this.ratio[1])));
+			let sizeFromWidth = this.contWidth/((3*this.nbCol+1)/2);
+			let sizeFromHeight = this.contHeight/((3*Math.ceil(data.length/this.nbCol)+3)/2);
+			if(sizeFromWidth<sizeFromHeight)
+			{
+				this.size = sizeFromWidth;
+				this.margin = this.size/2;
+				this.width = this.contWidth;
+				this.height = this.margin + Math.ceil(data.length/this.nbCol)*(this.size+this.margin);
+			}
+			else
+			{
+				this.size = sizeFromHeight;
+				this.margin = this.size/2;
+				this.height = this.contHeight;
+				this.width = this.margin + this.nbCol*(this.size+this.margin);
+			}
+
+			this.offsetX = (this.contWidth-this.width)/2 ;
+			this.offsetY = (this.contHeight-this.height)/2;
+			this.bSize = 0.04*this.height;
+			this.bPadding = 0.01*this.height;
+
+			let topBar = document.getElementById("topBar");
+			topBar.style.width = this.width-29+"px";
+			topBar.style.marginLeft = this.offsetX+1.5*this.padding+"px";
+			topBar.style.marginTop = this.offsetY+1.5*this.padding+"px";
+			this.rescaleContent(this);
+
+			this.update(this);
+		}
+
+
+		rescaleContent(phone)
+		{
+			let contentDiv = document.getElementById("content");
+			contentDiv.style.width = this.width-2*this.padding+"px";
+			//contentDiv.style.height = this.height-this.margin-this.padding+"px";
+			contentDiv.style.height = this.height-2*this.padding+"px";
+			contentDiv.style.marginLeft = this.offsetX+this.padding+"px";
+			contentDiv.style.marginTop = this.offsetY+this.padding+"px";
+			let frame = d3.selectAll(".frame").attr("height", this.height-2*this.bSize-2*this.bPadding);
+
+			let svg = d3.select('#'+this.container);
+
+			svg.selectAll("image").each(function() {
+				d3.select(this)
+				.attr("width", phone.size)
+				.attr("height", phone.size);
+			});
+
+		}
+
+
+		flipToPhone()
+		{
+			let parentDiv = document.getElementById(this.container).parentElement;
+			if(parentDiv.classList.contains("tabletFormat"))
+			{
+				this.empty();
+				parentDiv.classList.remove("tabletFormat");
+				parentDiv.classList.add("phoneFormat");
+				//We flip the ratio of the phone as well
+				this.ratio.reverse();
+				this.rescale();
+
+			}
+		}
+
+		flipToTablet()
+		{
+			let parentDiv = document.getElementById(this.container).parentElement;
+			if(parentDiv.classList.contains("phoneFormat"))
+			{
+				this.empty();
+				parentDiv.classList.remove("phoneFormat");
+				parentDiv.classList.add("tabletFormat");
+				//We flip the ratio of the phone as well
+				this.ratio.reverse();
+				this.rescale();
+			}
+		}
+
+		/*flip()
+		{
+		this.empty();
+		let parentDiv = document.getElementById(this.container).parentElement;
+		parentDiv.classList.toggle("phoneFormat");
+		parentDiv.classList.toggle("tabletFormat");
+		//We flip the ratio of the phone as well
+		this.ratio.reverse();
 		this.rescale();
-	}
+	}*/
 
-	rescale()
+	//Object passed as argument as this will not be useful in onClick responses
+	update(phone)
 	{
-		console.log("RESCALED");
-		this.contWidth = document.getElementById(this.container).clientWidth;
-		this.contHeight = document.getElementById(this.container).clientHeight;
-		this.nbCol=Math.round(this.ratio[1]*Math.sqrt(data.length/(this.ratio[0]*this.ratio[1])));
-		let sizeFromWidth = this.contWidth/((3*this.nbCol+1)/2);
-		let sizeFromHeight = this.contHeight/((3*Math.ceil(data.length/this.nbCol)+3)/2);
-		if(sizeFromWidth<sizeFromHeight)
-		{
-			this.size = sizeFromWidth;
-			this.margin = this.size/2;
-			this.width = this.contWidth;
-			this.height = this.margin + Math.ceil(data.length/this.nbCol)*(this.size+this.margin);
-		}
-		else
-		{
-			this.size = sizeFromHeight;
-			this.margin = this.size/2;
-			this.height = this.contHeight;
-			this.width = this.margin + this.nbCol*(this.size+this.margin);
-		}
 
-		this.offsetX = (this.contWidth-this.width)/2 ;
-		this.offsetY = (this.contHeight-this.height)/2;
-		this.bSize = 0.04*this.height;
-		this.bPadding = 0.01*this.height;
-
-		let topBar = document.getElementById("topBar");
-		topBar.style.width = this.width-29+"px";
-		topBar.style.marginLeft = this.offsetX+1.5*this.padding+"px";
-		topBar.style.marginTop = this.offsetY+1.5*this.padding+"px";
-		this.rescaleContent(this);
-
-		this.update(this);
-	}
-
-
-	rescaleContent(phone)
-	{
-		let contentDiv = document.getElementById("content");
-		contentDiv.style.width = this.width-2*this.padding+"px";
-		//contentDiv.style.height = this.height-this.margin-this.padding+"px";
-		contentDiv.style.height = this.height-2*this.padding+"px";
-		contentDiv.style.marginLeft = this.offsetX+this.padding+"px";
-		contentDiv.style.marginTop = this.offsetY+this.padding+"px";
-		let frame = d3.selectAll(".frame").attr("height", this.height-2*this.bSize-2*this.bPadding);
 
 		let svg = d3.select('#'+this.container);
-
-		svg.selectAll("image").each(function() {
-			d3.select(this)
-			.attr("width", phone.size)
-			.attr("height", phone.size);
-		});
-
-	}
+		let borderPad=2.5;
 
 
-	flipToPhone()
-	{
-		let parentDiv = document.getElementById(this.container).parentElement;
-		if(parentDiv.classList.contains("tabletFormat"))
+		//Appends app icon when new app enters
+		function appendIcon(phone, icon)
 		{
-			this.empty();
-			parentDiv.classList.remove("tabletFormat");
-			parentDiv.classList.add("phoneFormat");
-			//We flip the ratio of the phone as well
-			this.ratio.reverse();
-			this.rescale();
-
+			if(icon)
+			{
+				svg.append("defs")
+				.append("pattern")
+				.attr("width", 1)
+				.attr("height", 1)
+				.attr("id", 'icon_'+icon)
+				.append("image")
+				.attr("width", phone.size)
+				.attr("height", phone.size)
+				.attr("xlink:href", 'img/'+icon)
+				return 'url(#icon_'+icon+')';
+			}
+			//If no icon file given, we fill in grey
+			return 'grey';
 		}
-	}
-
-	flipToTablet()
-	{
-		let parentDiv = document.getElementById(this.container).parentElement;
-		if(parentDiv.classList.contains("phoneFormat"))
-		{
-			this.empty();
-			parentDiv.classList.remove("phoneFormat");
-			parentDiv.classList.add("tabletFormat");
-			//We flip the ratio of the phone as well
-			this.ratio.reverse();
-			this.rescale();
-		}
-	}
-
-	/*flip()
-	{
-	this.empty();
-	let parentDiv = document.getElementById(this.container).parentElement;
-	parentDiv.classList.toggle("phoneFormat");
-	parentDiv.classList.toggle("tabletFormat");
-	//We flip the ratio of the phone as well
-	this.ratio.reverse();
-	this.rescale();
-}*/
-
-//Object passed as argument as this will not be useful in onClick responses
-update(phone)
-{
 
 
-	let svg = d3.select('#'+this.container);
-	let borderPad=2.5;
-
-
-	//Appends app icon when new app enters
-	function appendIcon(phone, icon)
-	{
-		if(icon)
-		{
-			svg.append("defs")
-			.append("pattern")
-			.attr("width", 1)
-			.attr("height", 1)
-			.attr("id", 'icon_'+icon)
-			.append("image")
-			.attr("width", phone.size)
-			.attr("height", phone.size)
-			.attr("xlink:href", 'img/'+icon)
-			return 'url(#icon_'+icon+')';
-		}
-		//If no icon file given, we fill in grey
-		return 'grey';
-	}
-
-
-	//App rectangle
-	svg.selectAll('rect')
-	.data(this.data)
-	.enter()
-	.append('rect')
-	.attr('x', (d,i) => this.offsetX + this.margin + (i%this.nbCol)*(this.size+this.margin))
-	.attr('y', (d,i) => this.offsetY + this.margin/2 + this.topBarHeight + Math.floor(i/this.nbCol)*(this.size+this.margin))
-	.attr('rx', 0.3*this.size)
-	.attr('ry', 0.3*this.size)
-	.attr('width', this.size)
-	.attr('height', this.size)
-	.attr("fill", (d) => appendIcon(phone, d.icon))
-	.attr('filter', (d,i)=> i>this.idDiscover ? 'url(#grayscale)' :'')
-	.attr('class', (d,i)=> i>this.idDiscover ?
-				'temporary unavailable' : 'borderHighlight temporary')
-	.style('visibility', (d) => d.icon? "visible" : "hidden")
+		//App rectangle
+		svg.selectAll('rect')
+		.data(this.data)
+		.enter()
+		.append('rect')
+		.attr('x', (d,i) => this.offsetX + this.margin + (i%this.nbCol)*(this.size+this.margin))
+		.attr('y', (d,i) => this.offsetY + this.margin/2 + this.topBarHeight + Math.floor(i/this.nbCol)*(this.size+this.margin))
+		.attr('rx', 0.3*this.size)
+		.attr('ry', 0.3*this.size)
+		.attr('width', this.size)
+		.attr('height', this.size)
+		.attr("fill", (d) => appendIcon(phone, d.icon))
+		.attr('filter', (d,i)=> i>this.idDiscover ? 'url(#grayscale)' :'')
+		.attr('class', (d,i)=> i>this.idDiscover ?
+		'temporary unavailable' : 'borderHighlight temporary')
+		.style('visibility', (d) => d.icon? "visible" : "hidden")
 
 
 
-	/*.on("mouseover", function (d, i) {
-	// if size isn't passed as argument "this.width" should
-	// be used, but if we hover too quickly the transition
-	// doesn't happen and the size doesn't change
+		/*.on("mouseover", function (d, i) {
+		// if size isn't passed as argument "this.width" should
+		// be used, but if we hover too quickly the transition
+		// doesn't happen and the size doesn't change
+		d3.select(this).transition()
+		.duration(200)
+		//.attr('width', phone.size * 1.2)
+		//.attr('height', phone.size * 1.2)
+		.style("fill", "orange");
+	})
+
+	.on("mouseout", function (d, i) {
 	d3.select(this).transition()
 	.duration(200)
-	//.attr('width', phone.size * 1.2)
-	//.attr('height', phone.size * 1.2)
-	.style("fill", "orange");
-})
-
-.on("mouseout", function (d, i) {
-d3.select(this).transition()
-.duration(200)
-.attr('width', phone.size)
-.attr('height', phone.size)
-.style("fill", "gray");
-this.active = false;
+	.attr('width', phone.size)
+	.attr('height', phone.size)
+	.style("fill", "gray");
+	this.active = false;
 })*/
 
 .on('click', function (d,i){
