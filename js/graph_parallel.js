b142cfa6b80e7e6ffc15bf10f2206059413a7c36
ahmedkooli <ahmed.kooli@epfl.ch>
2020-05-25 22:54:18 +0200

    wikiencoremieux

--- js/graph_parallel.js
+++ js/graph_parallel.js
@@ -1,5 +1,5 @@
 var margin = {top: 50, right: 50, bottom: 50, left: 50},
-width = 960 - margin.left - margin.right,
+width = 750 - margin.left - margin.right,
 height = 500 - margin.top - margin.bottom;
 
 var dimensions = [
